11760653ae0aa19f71bb29f068b391b8e70b64fc
Daksh5074 <daksh5074@gmail.com>
2025-02-11 15:54:58 +0530

    lec 5

---
+++ Lec5-11-2-25/script.js
@@ -0,0 +1,120 @@
+// // // console.log(a);
+// // // var a=10;
+// // // function asumOfTwo(a,b){
+// // //     var sum =a+b;
+// // //     return sum;
+
+// // // }
+// // // console.log(asumOfTwo(10,9));
+// // // console.log(a);
+
+// // //setInterval
+// // function two(){
+// //     return 2;
+// // }
+// // function four(){
+// //     return 4;
+// // }
+// // console.log(1);
+// // console.log(two());
+// // setInterval(()=>{
+// //     console.log(3)
+
+// // },3000)
+// // console.log(four());
+// // setInterval(()=>{
+// //     console.log(5)
+
+// // },5000)
+// // console.log(6)
+
+
+
+
+//     for (var i=1;i<=6;i++){
+//        function print(i){
+//         setTimeout(()=>{
+//         console.log(i);
+//     },1000*i); 
+// }
+// print(i);
+// }
+
+
+
+// function z(){
+//     var b =900;
+//     function x(){
+//         var a=7;
+//         function y(){
+//             console.log(a,b);
+//         }
+//         y();
+//     }
+//     x();
+// }
+// z()
+
+
+
+
+// function inc() {
+//     var count = 0;
+
+//     return function increment() {
+//         count++;
+//         console.log(count);
+//     }
+//     // increment();
+//     // increment();
+//     // increment();
+//     // console.log(count);
+
+// }
+
+
+
+
+// // me();
+// // console.log(me());
+
+
+
+
+
+
+// '''''''''''''''''''''''''''''''''''''''''
+
+// function counter(){
+//     var count=0;
+//     return function increment(){
+//         count++;
+//         console.log(count);
+//     }
+    
+// }
+
+
+// var counter1=counter();
+// counter1();
+// counter1();
+// counter1();
+// counter1();
+
+
+
+function counter(){
+    var count=0;
+    this.increment=function(){
+        count++;
+        console.log(count);
+    }
+    this.decrement=function(){
+        count--;
+    console.log(count);
+}
+}
+var counter1=new counter();
+counter1.increment();
+counter1.decrement();
+counter1.increment();
